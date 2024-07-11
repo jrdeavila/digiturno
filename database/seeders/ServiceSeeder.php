80a738bad3a33936eab6d530c3b610d9ed7683f9
jrdeavila <jrdeavila@unicesar.edu.co>
2024-07-11 17:24:22 -0500

    feat: Update service seeder to use new services data
    Update the ServiceSeeder class to use the new services data in the database/seeders/data/services.php file. This ensures that the services are correctly seeded into the database when running the seeder.

--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -12,17 +12,12 @@ class ServiceSeeder extends Seeder
      */
     public function run(): void
     {
-        $attentionProfiles = require __DIR__ . '/data/services.php';
+        $services = require __DIR__ . '/data/services.php';
 
-        foreach ($attentionProfiles as $key => $value) {
+        foreach ($services as $service) {
             $ap = \App\Models\Service::create([
-                'name' => $key,
+                'name' => $service,
             ]);
-            for ($i = 0; $i < count($value); $i++) {
-                $ap->subservices()->create([
-                    'name' => $value[$i]
-                ]);
-            }
         }
     }
 }
