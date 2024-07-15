7a7014c756af68e6ead90e90bdc529f387a329e5
jrdeavila <jrdeavila@unicesar.edu.co>
2024-07-15 17:25:47 -0500

    feat: Add welcome message to the root route
    Add a welcome message to the root route of the application. This message will be returned as a JSON response when accessing the root URL. This provides a friendly greeting to users visiting the API.

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Http\JsonResponse;
 
 Route::get('/report', \App\Http\Controllers\ReportController::class)->name('report');
+
+
+Route::get('/', function () {
+  return new JsonResponse(['message' => 'Welcome to the API']);
+});
